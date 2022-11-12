fc847f5cdf33d4585bcef8dcd9a938d7cea8fccd
luchost <luchost@abv.bg>
2022-11-13 00:14:07 +0200

    update strings

--- Assets/Resources/Strings.cs
+++ Assets/Resources/Strings.cs
@@ -6,18 +6,19 @@ public static class Strings
 {
     public static string Get(string key) { return values[key]; }
 
-    public static readonly string[] weaponNames = {"fireball", "sword"};
-    public static readonly string[] levelNames = {"test1", "test2", "test3"};
-    public static readonly string[] bossNames = {"boss1", "boss2", "boss3"};
+    public static readonly string[] weaponNames = { "fireball", "sword" };
+    public static readonly string[] levelNames = { "test1", "test2", "test3" };
+    public static readonly string[] bossNames = { "boss1", "boss2", "boss3" };
 
     private static readonly Dictionary<string, string> values = new Dictionary<string, string>() {
-        {"demo_day_summary_1", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..."},
-        {"demo_day_summary_2", "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."},
-        {"demo_day_summary_3", "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+        {"demo_day_summary_1", "Man I feel amazing. Usane Bolt won't be able to out run me around town today."},
+        {"demo_day_summary_2", "Today is gonna be a good day. Hope I will have time for everything I want to do."},
+        {"demo_day_summary_3", "Ugh! Can't I sleep a little longer? F*ck, no, I can't. Work is waiting/calling."},
+        {"demo_day_summary_4", "Where am I? In the middle of nowhere. God f*cking damit! Kill me! I have to get back home again."},
 
-        {"demo_headline_1", "Sed ut perspiciatis unde omnis iste"},
-        {"demo_headline_2", "Nemo enim ipsam voluptatem quia voluptas"},
-        {"demo_headline_3", "Neque porro quisquam est"},
+        {"demo_headline_1", "A man walking his dog gets hit by a brick dropped by an attack eagle."},
+        {"demo_headline_2", "Last night a man has been attacked by a zombie and found dead in a small alley"},
+        {"demo_headline_3", "A women found dead after aliens try to abduct her!"},
         {"demo_headline_4", "Ut enim ad minima veniam, quis nostrum"},
         {"demo_headline_5", "Quis autem vel eum iure reprehenderit qui"},
         {"demo_headline_6", "At vero eos et accusamus et iusto"},
